93adb1a0dac87314516ca0d20068e47a08c959e9
lichao201626 <824683639@qq.com>
2020-05-07 10:29:55 +0800

    change html template

--- server/server.go
+++ server/server.go
@@ -24,7 +24,7 @@ func HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("r.URL = ", r.URL)
 	fmt.Println("r.Header = ", r.Header)
 	fmt.Println("r.Body = ", r.Body)
-	t, _ := template.ParseFiles("../template/login.template")
+	t, _ := template.ParseFiles("../template/login.html")
 	t.Execute(w, "")
 	// t.Execute(w, []string{"bgbiao", "biaoge"})
 	// fmt.Fprintf(w, t)
@@ -32,7 +32,7 @@ func HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
 
 func UserLoginHandler(response http.ResponseWriter, request *http.Request) {
 	fmt.Println("Handler Hello")
-	t, _ := template.ParseFiles("../template/notfound.template")
+	t, _ := template.ParseFiles("../template/notfound.html")
 	t.Execute(response, "")
 	// fmt.Fprintf(response, "Login Success")
 }
